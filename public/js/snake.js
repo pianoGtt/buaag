7ddd30145f7ce1e5691d900e263988c19b2e28dd
pianoGtt <171910543@qq.com>
2018-10-26 20:13:28 +0800

    123123

--- public/js/snake.js
+++ public/js/snake.js
@@ -15,14 +15,17 @@ var isStart = false;		//是否已经开始游戏
 var currentDirection = 38;	//当前方向
 var cW = 500;				//画布宽度 需要跟外面canvas统一
 var cH = 500;				//画布高度 需要跟外面canvas统一
-var loop;
+var loop;					//循环体
+var headColor = '#F8CD76';	//蛇头颜色
+var bodyColor = '#75F94C';	//蛇身颜色
+var foodColor = '#FFFFFF';	//食物颜色
+var lock = false;			//卡键锁死
 
 //方向 37左 38上 39右 40下
 var allowDirection = [37, 38, 39, 40];
 
 //初始蛇身
-var snakeBody = [[X, Y], [X, Y+P], [X, Y+P+P],
-	[X, Y+P+P+P],[X, Y+P+P+P+P],[X, Y+P+P+P+P+P]];
+var snakeBody = [[X, Y], [X, Y+P], [X, Y+P*2], [X, Y+P*3], [X, Y+P*4]];
 
 //初始食物
 var food = [];
@@ -39,7 +42,8 @@ window.onkeyup = function(key)
 		{
 			isStart = true;
 			alert('开始游戏');
-			loop = setInterval(start, speed);
+			createFood();
+			loop = setInterval(move, speed);
 		}
 	}
 	else
@@ -51,103 +55,6 @@ window.onkeyup = function(key)
 	}
 }
 
-//执行游戏
-function start()
-{
-	createFood();
-	move();
-	detectLevel();
-}
-
-//初始蛇的位置
-function initSnake()
-{
-	brush.fillStyle = '#75F94C';
-
-	//绘制新的蛇身
-	for(i=0;i<=snakeBody.length - 1;i++)
-	{
-		brush.rect(snakeBody[i][0], snakeBody[i][1], P, P);
-	}
-	brush.fill();
-}
-
-//画蛇
-function drawSnake(mX = 0, mY = 0)
-{
-	brush.fillStyle = '#75F94C';
-
-	//复制一份蛇身体,原身体用于清除原先点位
-	let tempShakeBody = JSON.parse(JSON.stringify(snakeBody));
-
-	//删掉蛇尾
-	brush.clearRect(snakeBody[snakeBody.length-1][0], snakeBody[snakeBody.length-1][1], P, P);
-
-	//绘制新的蛇身
-	for(i=0;i<=snakeBody.length - 1;i++)
-	{
-		if(i === 0)
-		{
-			snakeBody[i][0] += mX;
-			snakeBody[i][1] += mY;
-			brush.fillRect(snakeBody[i][0], snakeBody[i][1], P, P);
-		}
-		else
-		{
-			snakeBody[i][0] = tempShakeBody[i-1][0];
-			snakeBody[i][1] = tempShakeBody[i-1][1];
-		}
-	}
-}
-
-//添加蛇身
-function add()
-{
-	let sw = snakeBody[snakeBody.length - 1];
-	let sw2 = snakeBody[snakeBody.length - 2];
-
-	let pX = sw[0] - sw2[0] + sw[0];
-	let pY = sw[1] - sw2[1] + sw[1];
-
-	snakeBody.push([pX, pY]);
-	food.pop();
-
-	score += 1;
-	document.getElementById('score').innerHTML = score;
-}
-
-//改变方向
-function changeDirection(direction)
-{
-	switch(currentDirection)
-	{
-		case 37:
-			if(direction === 38 || direction === 40)
-			{
-				currentDirection = direction;
-			}
-			break;
-		case 38:
-			if(direction === 37 || direction === 39)
-			{
-				currentDirection = direction;
-			}
-			break;
-		case 39:
-			if(direction === 38 || direction === 40)
-			{
-				currentDirection = direction;
-			}
-			break;
-		case 40:
-			if(direction === 37 || direction === 39)
-			{
-				currentDirection = direction;
-			}
-			break;
-	}
-}
-
 //移动
 function move()
 {
@@ -155,25 +62,21 @@ function move()
 	if(currentDirection === 37)
 	{
 		drawSnake(-P);
-		X -= P;
 	}
 	//上移动
 	else if(currentDirection === 38)
 	{
 		drawSnake(0 , -P);
-		Y -= P;
 	}
 	//右移动
 	else if(currentDirection === 39)
 	{
 		drawSnake(P);
-		X += P;
 	}
 	//下移动
 	else if(currentDirection === 40)
 	{
 		drawSnake(0 , P);
-		Y -= P;
 	}
 
 	//判断是否吃豆
@@ -195,6 +98,123 @@ function move()
 	isKillSelf();
 }
 
+//初始蛇的位置
+function initSnake()
+{
+	//绘制新的蛇身
+	for(i=0;i<=snakeBody.length - 1;i++)
+	{
+		if(i === 0)
+		{
+			brush.fillStyle = headColor;
+			brush.fillRect(snakeBody[i][0], snakeBody[i][1], P, P);
+		}
+		else
+		{
+			brush.fillStyle = bodyColor;
+			brush.fillRect(snakeBody[i][0], snakeBody[i][1], P, P);
+		}
+	}
+}
+
+//画蛇
+function drawSnake(mX = 0, mY = 0)
+{
+	//复制一份蛇身体,原身体用于清除原先点位
+	let tempShakeBody = JSON.parse(JSON.stringify(snakeBody));
+
+	//删掉蛇尾
+	brush.clearRect(snakeBody[snakeBody.length-1][0], snakeBody[snakeBody.length-1][1], P, P);
+
+	//绘制新的蛇身
+	for(i=0;i<=snakeBody.length - 1;i++)
+	{
+		if(i === 0)
+		{
+			brush.fillStyle = headColor;
+			snakeBody[i][0] += mX;
+			snakeBody[i][1] += mY;
+			brush.fillRect(snakeBody[i][0], snakeBody[i][1], P, P);
+		}
+		else
+		{
+			brush.fillStyle = bodyColor;
+			snakeBody[i][0] = tempShakeBody[i-1][0];
+			snakeBody[i][1] = tempShakeBody[i-1][1];
+			brush.fillRect(snakeBody[i][0], snakeBody[i][1], P, P);
+		}
+	}
+
+	lock = false;
+}
+
+//添加蛇身
+function add()
+{
+	let sw = snakeBody[snakeBody.length - 1];
+	let sw2 = snakeBody[snakeBody.length - 2];
+
+	let pX = sw[0] - sw2[0] + sw[0];
+	let pY = sw[1] - sw2[1] + sw[1];
+
+	snakeBody.push([pX, pY]);
+	food.pop();
+
+	score += 1;
+	document.getElementById('score').innerHTML = score;
+
+	detectLevel();
+	createFood();
+}
+
+//移动
+function changeDirection(direction)
+{
+	switch(currentDirection)
+	{
+		case 37:
+			if(direction === 38 || direction === 40)
+			{
+				if(lock === false)
+				{
+					currentDirection = direction;
+					lock = true;
+				}
+			}
+			break;
+		case 38:
+			if(direction === 37 || direction === 39)
+			{
+				if(lock === false)
+				{
+					currentDirection = direction;
+					lock = true;
+				}
+			}
+			break;
+		case 39:
+			if(direction === 38 || direction === 40)
+			{
+				if(lock === false)
+				{
+					currentDirection = direction;
+					lock = true;
+				}
+			}
+			break;
+		case 40:
+			if(direction === 37 || direction === 39)
+			{
+				if(lock === false)
+				{
+					currentDirection = direction;
+					lock = true;
+				}
+			}
+			break;
+	}
+}
+
 //是否自杀
 function isKillSelf()
 {
@@ -202,6 +222,7 @@ function isKillSelf()
 	{
 		if(snakeBody[0][0] === snakeBody[i][0] && snakeBody[0][1] === snakeBody[i][1])
 		{
+			clearInterval(loop);
 			alert('你啃到自己了,最终得分:' + score);
 			reset();
 		}
@@ -211,8 +232,9 @@ function isKillSelf()
 //是否撞墙
 function isOutSide()
 {
-	if(snakeBody[0][0] < 0 || snakeBody[0][1] < 0 || snakeBody[0][0] > cW || snakeBody[0][1] > cH)
+	if(snakeBody[0][0] < 0 || snakeBody[0][1] < 0 || snakeBody[0][0] > (cW - P) || snakeBody[0][1] > (cH - P))
 	{
+		clearInterval(loop);
 		alert('你撞墙上了,最终得分:' + score);
 		reset();
 	}
@@ -234,7 +256,7 @@ function createFood()
 		//没有被占用颜色格子的才能生成食物
 		if(red === 0 && green === 0 && blue === 0)
 		{
-			brush.fillStyle = '#fff';
+			brush.fillStyle = foodColor;
 			brush.fillRect(fX, fY, P, P);
 
 			food.push([fX, fY]);
@@ -249,41 +271,46 @@ function createFood()
 //重置游戏
 function reset()
 {
-	clearInterval(loop);
 	return window.location.reload();
 }
 
 //检测级别
 function detectLevel()
 {
-	if(score > 10 && score < 20 && level !== 2)
+	if(score >= 6 && score <= 10 && level !== 2)
 	{
 		level = 2;
 		document.getElementById('level').innerHTML = level;
 
-		speed /= 2;
-
+		speed -= 20;
 		clearInterval(loop);
-		loop = setInterval(start, speed);
+		loop = setInterval(move, speed);
 	}
-	else if(score > 20 && score < 30 && level !== 3)
+	else if(score >= 11 && score <= 15 && level !== 3)
 	{
 		level = 3;
 		document.getElementById('level').innerHTML = level;
 
-		speed /= 2;
-
+		speed -= 20;
 		clearInterval(loop);
-		loop = setInterval(start, speed);
+		loop = setInterval(move, speed);
 	}
-	else if(score > 30 && score < 40 && level !== 4)
+	else if(score >= 16 && score <= 20 && level !== 4)
 	{
 		level = 4;
 		document.getElementById('level').innerHTML = level;
 
-		speed /= 2;
-
+		speed -= 20;
 		clearInterval(loop);
-		loop = setInterval(start, speed);
+		loop = setInterval(move, speed);
+	}
+	else if(score >= 21 && level !== 5)
+	{
+		level = 5;
+		document.getElementById('level').innerHTML = level;
+
+		speed -= 20;
+		clearInterval(loop);
+		loop = setInterval(move, speed);
 	}
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE HTML>
 <!--@author yang-->
+<!--v1.1-->
+<!--修复按快了以后自杀问题,优化了移动算法-->
 <html>
-    <head>
-        <title>贪吃蛇</title>
-        <link rel="stylesheet" type="text/css" href="/css/snake.css">
-    </head>
-    <body>
-        <canvas id="canvas" width="500" height="500"></canvas>
-            <div id="markpad">
-                <p>Score:<span id="score">0</span></p>
-                <p>Level:<span id="level">1</span></p>
-                <p><font size="1">按回车开始游戏 10-20分2级 20-30分3级 30+ 4级</font></p>
-                <p><font size="1">操作说明：小键盘 ↑↓←→</font></p>
-            </div>
-        <script src="/js/snake.js" type="text/javascript"></script>
-    </body>
+	<head>
+		<title>贪吃蛇</title>
+		<link rel="stylesheet" type="text/css" href="./snake.css">
+	</head>
+	<body>
+		<canvas id="canvas" width="500" height="500"></canvas>
+		<div id="markpad">
+			<p>Score:<span id="score">0</span></p>
+			<p>Level:<span id="level">1</span></p>
+			<p><font size="1">按回车开始游戏 10-20分2级 20-30分3级 30+ 4级</font></p>
+			<p><font size="1">操作说明：小键盘 ↑↓←→</font></p>
+		</div>
+		<script src="./snake.js" type="text/javascript"></script>
+	</body>
 </html>
